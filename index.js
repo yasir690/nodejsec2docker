992a51e4edc261b9a15a6d0a2c5f9252091232c0
yasir690 <yasirsaleemrajput@gmail.com>
2024-11-23 04:34:19 +0500

    change

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.get('/',(req,res)=>{
     res.send('servers is running at 4000')
 })
 
-app.listen(port,()=>{
+app.listen(port,'0.0.0.0',()=>{
     console.log('server is running at 4000 port');
     
 })
